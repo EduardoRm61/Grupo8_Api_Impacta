2a306626f6ec6c61251b092ea21fd738b1116f40
Eduardo N. <eduardonunesdasilva23@gmail.com>
2025-03-14 19:01:53 -0300

    Criando novas execessões e dados de professor teste

--- dadosPro.py
+++ dadosPro.py
@@ -1,22 +1,11 @@
 #import professor as prof
 
-dadosTurma = {"Turma":[
-    {"Id": 12, "Descrição": "Eng. Software","Ativa": True,"Professor Id": dados.prof},
-    {"Id": 14, "Descrição": "Análise e Desen. de Sistemas", "Ativa": False, "Professor Id": dados.prof}     #o Id do professor não estou desenvolvendo, quando estiver pronto eu importareo
+dadosPro = {"Professor":[
+    {"Id": 12, "Nome": "Caio"},
+    {"Id": 15, "Nome": "Furlan"}
 ]}
 
-class ProfessorNaoIdentificado(Exception):
-    def __init__(self,msg="Erro, Professor não indentificado ou inexistente"):
-        self.msg = msg
-        super().__init__(self.msg)
-
-def listProfessor(id_Professor):
-    id_Profs = [professor]              # Aqui eu puxo de dados, onde estará professor.
-    for Ids in id_Profs:
-        if Ids["Id"] == id_Professor:
-            return Ids
-        raise ProfessorNaoIdentificado()
-    
-        
-def addProfessor(novo_dict):
-    return dadosTurma["Turma"].append(novo_dict)
+dadosTurma = {"Turma":[
+    {"Id": 12, "Descrição": "Eng. Software","Ativa": True,"Professor Id": 15 },
+    {"Id": 14, "Descrição": "Análise e Desen. de Sistemas", "Ativa": False, "Professor Id": 12 }   
+]}
--- turma.py
+++ turma.py
@@ -2,10 +2,32 @@ from flask import Flask, jsonify, request
 import dadosPro 
 app = Flask(__name__)
 
-dadosTurma = {"Turma":[
-    {"Id": 12, "Descrição": "Eng. Software","Ativa": True,"Professor Id": 14},
-    {"Id": 14, "Descrição": "Análise e Desen. de Sistemas", "Ativa": False, "Professor Id": 13}
+dadosPro = {"Professor":[
+    {"Id": 12, "Nome": "Caio"},
+    {"Id": 15, "Nome": "Furlan"}
 ]}
+
+dadosTurma = {"Turma":[
+    {"Id": 12, "Descrição": "Eng. Software","Ativa": True,"Professor Id": 12},
+    {"Id": 14, "Descrição": "Análise e Desen. de Sistemas", "Ativa": False, "Professor Id": 15}     
+]}
+
+ # Criando Classes de excessões 
+
+class ProfessorNaoIdentificado(Exception):
+    def __init__(self,msg="Erro, Professor não indentificado ou inexistente!"):
+        self.msg = msg
+        super().__init__(self.msg)
+
+class TurmaNaoIdentificada(Exception):
+    def __init__(self, msg="Erro, Turma não identificada ou inexistente!"):
+        self.msg = msg
+        super().__init__(self.msg)
+
+class TurmaExistente(Exception):
+    def __init__(self, msg="Erro, Turma já existente!"):
+        self.msg = msg
+        super().__init__(self.msg)
 
 
 # Aqui estão as rotas para as determinadas requisições Http
@@ -21,10 +43,22 @@ def procurarTurma(id_turma):
     try:
         
 
-
-
 @app.route("/Turma.criar", methods=["POST"])
 def criarTurma():
     novo_dict = request.jsonify
 
+class ProfessorNaoIdentificado(Exception):
+    def __init__(self,msg="Erro, Professor não indentificado ou inexistente!"):
+        self.msg = msg
+        super().__init__(self.msg)
 
+def listProfessor(id_Professor):
+    id_Profs = [professor]              # Aqui eu puxo de dados, onde estará professor.
+    for Ids in id_Profs:
+        if Ids["Id"] == id_Professor:
+            return Ids
+        raise ProfessorNaoIdentificado()
+    
+        
+def addProfessor(novo_dict):
+    return dadosTurma["Turma"].append(novo_dict)
