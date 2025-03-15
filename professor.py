c17ddbc4e23d4271b5e547f4f2e09039682e4a20
Alessandra F Rigonatti <alessandra.rigonatti@aluno.faculdadeimpacta.com.br>
2025-03-15 01:01:15 -0300

    professor.py - class professoor_not_found e lista professores

---
+++ professor.py
@@ -0,0 +1,12 @@
+dados = {"alunos":[], 
+        "professores":[
+            {"ide": 10, "nome": "Caio", "idade": 27, "matéria": "Dev API E Micros", "obsercacoes": "Contato com aulo via Chat"}
+            {"ide": 11, "nome": "Odair", "idade": 30, "matéria": "- DevOps", "obsercacoes": }
+            
+        ]}
+class Professor_Not_found(Exception):
+    pass
+    #pelo que me lembre pass é para seguir com o cód sem ter uma ação | se o professor não for encontrado pula cód a seguir 
+    #fora desta classe
+    
+    def  
