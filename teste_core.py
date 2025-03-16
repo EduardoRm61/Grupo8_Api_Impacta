341c99128e57ca1704211d4090b3657348d71834
Alessandra F Rigonatti <alessandra.rigonatti@aluno.faculdadeimpacta.com.br>
2025-03-16 13:51:20 -0300

    Arumando Teste_get_prof | test_get_all | teste get existir

--- teste_core.py
+++ teste_core.py
@@ -1,31 +1,33 @@
-import unittest        #import, não fr
+import unittest
 from app import app
 
 class Teste_GET_Professor(unittest.TestCase):
+    '''Classe apara analise da rota GET - retorno dos dados da lista'''
 
     def setUp(self):
+        '''Cliente = dict que simula um cliente real '''
         self.app = app.test_client()
+        self.professores_iniciais = {
+            "professor": [
+                {"id": 10, "nome": "Caio", "idade": 27, "materia": "Dev API E Micros", "observacoe": "Contato com aluno via Chat"},
+                {"id": 11, "nome": "Odair", "idade": 30, "materia": "DevOps", "observacoe": None}
+            ]
+        }
+        app.config['PROFESSORES'] = self.professores_iniciais.copy()
 
-    def test_get(self):
+    def test_get_all_professores(self):
+        
         response = self.app.get('/professores')
-        self.assertEqual(200, response.status_code)
-
-    def test_get_existing_user(self):
-        response = self.app.get('/professores/10')  
-        data = response.get_json()  
-        self.assertEqual(dict, type(data))
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(data["id"], 10)
-        self.assertEqual(data["nome"], 'Caio')
-        self.assertEqual(data["idade"], 27)
-        self.assertEqual(data["materia"], "Dev API E Micros")
-        self.assertEqual(data["observacoe"], "Contato com aluno via Chat")
-   
-        response = self.app.delete('/professsores/10')
+        data = response.get_json() #variável data/ que poderia ser dados, recebe a resposta do retorno/get em json e transforma em .py
+        self.assertEqual(data, self.professores_iniciais)
+
+    def test_get_existing_professor(self):
+        '''GET por id específico, "individual" '''
+        response = self.app.get('/professores/10')
         self.assertEqual(response.status_code, 200)
         data = response.get_json()
-        self.assertEqual(data["id"], 10)
-
+        self.assertEqual(data, self.professores_iniciais["professor"][0])  #acessa índice 0 da lista (1° chave)
 
 if __name__ == "__main__":
     unittest.main()
