fcc93a8d46205493ef02aaa592e043baae340f41
Thaina <thaina.lima@aluno.faculdadeimpacta.com.br>
2025-03-16 16:12:25 -0300

    arrumei a identação do app.py

--- app.py
+++ app.py
@@ -17,15 +17,18 @@ def cadastrar_professores():
     professores.append(novo_professor)
     return jsonify(novo_professor), 201
     
-    @app.route('/profesores/<int:id>', methods=['PUT'])
-    def atualizar_professor(id):
+@app.route('/profesores/<int:id>', methods=['PUT'])
+def atualizar_professor(id):
         dados = request.json
         for professor in professores:
         f professor['id'] == id:
-            professor['nome'] = dados.get('nome', professor['nome'])
-            professor['disciplina'] = dados.get('disciplina', professor['disciplina'])
-            return jsonify(professor), 200
+        professor['nome'] = dados.get('nome', professor['nome'])
+        professor['disciplina'] = dados.get('disciplina', professor['disciplina'])
+        return jsonify(professor), 200
     return jsonify({'erro': 'Professor não encontrado'}), 404
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
