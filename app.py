916f6fe7767f044e540a75b0ffcb95fffbce5a78
Alessandra F Rigonatti <alessandra.rigonatti@aluno.faculdadeimpacta.com.br>
2025-03-17 11:52:26 -0300

    teste ok get prof | get_all | get_existente | nonexisted | ppo st |correção dos returns do app.py e teste_core.py - correção de sintaxe e ló gica

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ from flask import Flask, jsonify, request
 
 
 professores = {"professor":[
-            {"id": 10, "nome": "Caio", "idade": 27, "materia": "Dev API E Micros", "observacoe": "Contato com aluno via Chat"},     #lembrar da vírula
-            {"id": 11, "nome": "Odair", "idade": 30, "materia": "DevOps", "observacoe": None }   #correção do nome - Dev para apenas Dev = cuiaddo, dá erro
+            {"id": 10, "nome": "Caio", "idade": 27, "materia": "Dev API E Micros", "obs": "Contato com aluno via Chat"},     #lembrar da vírula  | troquei para obs, o nome dava muito erro
+            {"id": 11, "nome": "Odair", "idade": 30, "materia": "DevOps", "obs": None }   #correção do nome - Dev para apenas Dev = cuiaddo, dá erro
             ]
         }
 
@@ -16,7 +16,7 @@ def listar_professores():
     
     ''' Retorno dos dados da list/dict professor'''
     
-    return jsonify(professores), 200
+    return jsonify({"mensagem": "Ok", "professor": professores["professor"]}), 200  #faltou chamar a chave
 
 
 @app.route("/professores/<int:id>", methods=["GET"])
@@ -27,8 +27,8 @@ def pesquisa_professor(id):
             
     for professor in professores["professor"]:     #lembrar - chamar nome da lista e dentro do colchete o dado a ser acessado, neste caso dict professores
         if professor["id"] == id:
-            return jsonify(professor), 200
-        return jsonify("Not Found - Professor inexistente"), 404
+            return jsonify({"mensagem": "Ok", "professor": professor}), 200    # chave mensagem possui ok, chave professor possui dict professor NESTE CASO CHAMA DIRETO A CHAVE E NÃO O DICT
+        return jsonify({"error": "Not Found - Professor inexistente"}), 404     #Não vi que o do Edu estava como o meu, arrumando
 
         
 #observar cod no comentário a baixo
@@ -54,9 +54,8 @@ def cadastrar_professores():
    
     novo_professor = request.json
 
-    if not novo_professor or "id" not in novo_professor:
-        return jsonify("Id obrigatório"), 400
-
+    if not novo_professor or "nome" not in novo_professor or "materia" not in novo_professor:  #adicionando materia como campo obrigatório além do nome | correção do or após "nome", ele não existe
+        return jsonify({"error": "Id, nome e matéria são obrigatórios"}), 400
     '''pegar último, compara, adiciona 1 no índice, add o prof. Caso list vazia, inicia no índice 1 (trás pra frente)'''
     if professores["professor"]:
         ultimo_professor = professores["professor"][-1]
@@ -67,7 +66,7 @@ def cadastrar_professores():
     novo_professor["id"] = novo_id_prof
     professores["professor"].append(novo_professor)
 
-    return jsonify(novo_professor), 201
+    return jsonify({"mensagem": "Created", "professor": novo_professor}), 201    #retorna novo professor 
     
 @app.route('/profesores/<int:id>', methods=['PUT'])
 def atualizar_professor(id):
@@ -80,18 +79,18 @@ def atualizar_professor(id):
             professor['nome'] = professores.get('nome', professor['nome'])
             professor['idade'] = professores.get('idade', professor['idade'])
             professor['materia'] = professores.get('materia', professor['materia'])
-            professor['obsercacoes'] = professores.get('obsercacoes', professor['obsercacoes'])
-            return jsonify(professor), 200
-    return jsonify("Not Found - Professor inexistente"), 404
+            professor['obs'] = professores.get('obs', professor['obs'])
+            return jsonify({"mensagem": "Atualizado", "professor": professor}), 200  #não retorna a lista, apenas os dados da chave professor
+    return jsonify({"error": "Not Found - Professor inexistente"}), 404
 
 @app.route("/professsores/<int:id>", methods = ["DELETE"])
 
 def delete_professor(id):
-    for professor in professores:
+    for professor in professores["professor"]:   #lembrar que chamo 1° o dict e dentro do colchete[] chamo a chave
         if professor["id"] == id:
-            professores.remove(professor)
-            return jsonify(professor)
-    return jsonify("Not Found - Professor inexistente")
+            professores["professor"].remove(professor)   #mesmo erro da de cima dict ["chave"]
+            return jsonify({"mensagem": "Deletado", "professor": professor}) #apenas o prfessor deletado
+    return jsonify({"error": "Not Found - Professor inexistente"}), 404
 
 if __name__ == '__main__':
     app.run(debug=True)
--- teste_core.py
+++ teste_core.py
@@ -9,8 +9,8 @@ class Teste_GET_Professor(unittest.TestCase):
         self.app = app.test_client()
         self.professores_iniciais = {
             "professor": [
-                {"id": 10, "nome": "Caio", "idade": 27, "materia": "Dev API E Micros", "observacoe": "Contato com aluno via Chat"},
-                {"id": 11, "nome": "Odair", "idade": 30, "materia": "DevOps", "observacoe": None}       #cuiado com escrita, acentuação, podem sar erro
+                {"id": 10, "nome": "Caio", "idade": 27, "materia": "Dev API E Micros", "obs": "Contato com aluno via Chat"},
+                {"id": 11, "nome": "Odair", "idade": 30, "materia": "DevOps", "obs": None}       #cuiado com escrita, acentuação, podem dar erro
             ]
         }
         app.config['PROFESSORES'] = self.professores_iniciais.copy()
@@ -20,14 +20,17 @@ class Teste_GET_Professor(unittest.TestCase):
         response = self.app.get('/professores')
         self.assertEqual(response.status_code, 200)
         data = response.get_json() #variável data/ que poderia ser dados, recebe a resposta do retorno/get em json e transforma em .py
-        self.assertEqual(data, self.professores_iniciais)
+        self.assertEqual(data["mensagem"], "Ok")    
+        self.assertEqual(data["professor"], self.professores_iniciais["professor"])  #retorno de duas chaves - 1° mensagem ou erro com seus valores - 2° professor com chave professor
 
     def test_get_existing_professor(self):
         '''GET por id específico, "individual" '''
         response = self.app.get('/professores/10')
         self.assertEqual(response.status_code, 200)
         data = response.get_json()
-        self.assertEqual(data, self.professores_iniciais["professor"][0])  #acessa índice 0 da lista (1° chave)
+        self.assertEqual(data["mensagem"], "Ok")
+        self.assertEqual(data["professor"], self.professores_iniciais["professor"][0])  #acessa índice 0 da lista (1° chave)
+        #  [self.professores_iniciais["professor"][0]] porque é uma segunda lista
         
     def test_get_nonxistent_professor(self):
         '''Vê o erro de Not found professor'''
@@ -36,5 +39,26 @@ class Teste_GET_Professor(unittest.TestCase):
         data = response.get_json()
         self.assertIsNotNone("Not Found - Professor inexistente", data)
         #não havia colocado NotNone - outra falha era retorno do app.py
+        
+        
+    def test_post_professor(self):
+        '''Testa a rota POST /professores (adicionar novo professor)'''
+        novo_professor = {
+            "id": 12, 
+            "nome": "Gustavo",
+            "idade": None,
+            "materia": "Dev Mob",
+            "obs": "Sexta é dia da maldade"
+        }
+        response = self.app.post('/professores', json=novo_professor)
+        self.assertEqual(response.status_code, 201)
+        data = response.get_json()
+        self.assertEqual(data["mensagem"], "Created")     #cuidado em abrir e feichar colchetes estava [mensagem , creted]
+        self.assertEqual(data["professor"]["id"], novo_professor["id"])  
+        self.assertEqual(data["professor"]["nome"], novo_professor["nome"]) 
+        self.assertEqual(data["professor"]["materia"], novo_professor["materia"]) 
+        self.assertEqual(data["professor"]["obs"], novo_professor["obs"]) 
+        # comparando os dados da variável data [observacao] com o do novo_professor, ! NÃO COLOCA [-1] PORQUE É UM TESTE COM RETORNO DE CADASTRO "DIRETO"
+        
 if __name__ == "__main__":
     unittest.main()
