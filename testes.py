756265afa9ca1f8a8a04255be95bf03834fba7db
Eduardo N. <eduardonunesdasilva23@gmail.com>
2025-03-18 14:02:08 -0300

    Corrigi alguns problemas. O primeiro teste GET passou

--- testes.py
+++ testes.py
@@ -4,16 +4,17 @@ from app import app
 
 class TestStringMethods(unittest.TestCase):
 
-    def teste_000_lista_professor(self):
+    def teste_000_lista_professorGET(self):
         r = requests.get('http://localhost:5002/Turma')
 
         if r.status_code == 404:
-            self.fail("voce nao definiu a pagina /alunos no seu server")
-
+            self.fail("voce nao definiu a pagina /Turma no seu server")
         try:
             obj_return = r.json()
-
         except:
             self.fail("O Retorndo deve ser em JSON")
         self.assertEqual(type(obj_return),type([]))
 
+
+if __name__ == '__main__':
+    unittest.main()
