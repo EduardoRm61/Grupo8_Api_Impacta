10a692eb8d56e851c0abd37a408d5358860b6306
Eduardo N. <eduardonunesdasilva23@gmail.com>
2025-03-18 08:32:19 -0300

    Criando as primeiras classes de exceções

--- app.py
+++ app.py
@@ -10,3 +10,14 @@ dados = {
     ],
     "professores": []
 }
+
+class AlunoNaoIdentificado(Exception):
+    def __init__(self, msg="Erro, Aluno não identificado ou inexistente!"):
+        self.msg = msg
+        super().__init__(self.msg)
+
+class AlunoExistente(Exception):
+    def __init__(self, msg="Erro, Aluno já existente!"):
+        self.msg = msg
+        super().__init__(self.msg)
+
