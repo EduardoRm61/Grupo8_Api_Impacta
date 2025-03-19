24b1f8fe69ca3be66b51a7200f164e0f63588e03
Thainá Foltran <thainafoltran@hotmail.com>
2025-03-19 17:44:35 -0300

    corrigindo lista branch aluno para testar depois

--- app.py
+++ app.py
@@ -15,6 +15,17 @@ dados = {
             "Nota_Primeiro_Semestre": 8.0,
             "Nota_Segundo_semestre": 9.0,
             "Media_final": 8.5
+        },
+
+        {
+            "Id": 25,
+            "Nome": "Rafaela",
+            "Idade": 25,
+            "Turma_Id": 16,
+            "Data_nascimento": "10/09/2000",
+            "Nota_Primeiro_Semestre": 6.0,
+            "Nota_Segundo_semestre": 9.0, 
+            "Media_final": 7.5
         }
     ]
 }
