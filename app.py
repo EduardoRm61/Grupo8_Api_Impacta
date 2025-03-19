2a5e1b9d73da4ee9e6ab889b06a64cd5f7c0b5ce
Eduardo N. <eduardonunesdasilva23@gmail.com>
2025-03-19 00:19:44 -0300

    Organizando arquivo

--- app.py
+++ app.py
@@ -17,10 +17,23 @@ dados = {
 }
 
 
-dadosProfessor = {"Professor":[
-    {"Id": 12, "Nome": "Caio"},
-    {"Id": 15, "Nome": "Furlan"}
+
+
+professores = {"professor": [
+    {
+    "id": 10,
+    "nome": "Caio",
+    "idade": 27,
+    "materia": "Dev API E Micros",
+    "obs": "Contato com aluno via Chat"},
+
+    {"id": 11,
+    "nome": "Odair",
+    "idade": 30, 
+    "materia": "DevOps",
+    "obs": None}
 ]}
+
 
 # dadosTurma = {"Turma":[
 #     {"Id": 12, "Descrição": "Eng. Software","Ativa": True,"Professor Id": 12},
@@ -30,7 +43,7 @@ dadosProfessor = {"Professor":[
 dadosTurma = {"Turma":[     
 ]}
 
-#Criando classes para minhas excessões:
+#Criando todas as classes de exceções:
 class TurmaNaoIdentificada(Exception):
     def __init__(self, msg="Erro, Turma não identificada ou inexistente!"):
         self.msg = msg
@@ -61,7 +74,42 @@ class ValorBool(Exception):
         self.msg = msg
         super().__init__(self.msg)
 
-#Criando fuções para as requisições:
+class AlunoNaoIdentificado(Exception):
+    def _init_(self, msg="Erro, Aluno não identificado ou inexistente!"):
+        self.msg = msg
+        super()._init_(self.msg)
+
+class AlunoExistente(Exception):
+    def _init_(self, msg="Erro, Aluno já existente!"):
+        self.msg = msg
+        super()._init_(self.msg)
+
+class CadastroDeAlunoFalhado(Exception):
+    def _init_(self, msg="Erro, Id do aluno ou Turma_Id incorretos!"):
+        self.msg = msg
+        super()._init_(self.msg)
+
+class AtualizacaoAlunoFalhou(Exception):
+    def _init_(self, msg="Erro, Não foi possível atualizar os dados do aluno! Reveja os campos e preencha corretamente"):
+        self.msg = msg
+        super()._init_(self.msg)
+
+class ProfessorNaoIdentificado(Exception):
+    def __init__(self, msg="Not Found - Professor inexistente"):
+        self.msg = msg
+        super().__init__(self.msg)
+
+class ProfessorExiste(Exception):
+    def __init__(self, msg="Professor já existente"):
+        self.msg = msg
+        super().__init__(self.msg)
+
+class CadastroDeProfessorFalhado(Exception): # Correção: Nome da classe estava incorreto na chamada do except
+    def __init__(self, msg="ID, nome e matéria são obrigatórios"):
+        self.msg = msg
+        super().__init__(self.msg)
+
+#Criando funções para as requisições:
 
 def ProcurarTurmaPorId(id_turma):
             for dict in dadosTurma["Turma"]:
@@ -73,11 +121,13 @@ def CriarNovaTurma(nv_dict):
     dadosTurma["Turma"].append(nv_dict)
     return
 
-##############################
-# Função para todo o servidor:
-# def apaga_tudo():
-#     dados['alunos'] = []
-##############################
+
+def apaga_tudo():
+    dados['alunos'] = []
+    professores["Professor"] = []
+    dadosTurma["Turma"] = []
+
+
 def ListarTurma():
     return dadosTurma["Turma"]
 
@@ -96,7 +146,7 @@ def DeletarTurmaPorId(id_turma):
 
 
 def ProfessorExistente(Id_professor):
-    for professor in dadosProfessor["Professor"]:
+    for professor in professores["Professor"]:
         if professor["Id"] == Id_professor:
             return True  
     return False
@@ -140,17 +190,82 @@ def AlterarInformacoes(Id_turma, Descricao, Ativa, Id_Pro):
             "Erro": "Não foi possível fazer a requisição",
             "Descrição": str(e)
         }), 500
-                
- 
-#Criando as rotas, lembrando que a função jsonify só pode ser usada nas rotas:
+    
+# Aqui estão as funções auxiliares para as rotas:
 
-#################################################
-#Rota para apagar todo o servidor
-# @app.route("/reseta", methods=["POST","DELETE"])
-# def reseta():
-#     apaga_tudo()
-#     return "resetado" 
-##################################################
+def gerar_novo_id():
+    '''Criação de id, obrigatório'''
+    if not professores["professor"]: # Correção: A verificação deve ser na lista de professores
+        return 1
+    return max(professor["id"] for professor in professores["professor"]) + 1 # Correção: Iterar sobre a lista correta
+
+
+def procurarProfessorPorId(id_professor):   #def é minúscula
+    for professor in professores["professor"]:
+        if professor['id'] == id_professor:
+            return professor
+    raise ProfessorNaoIdentificado()
+
+def criarNovoProfessor(nv_dict):
+    professores["professor"].append(nv_dict)
+    return
+
+def deletarProfessorPorId(id_professor):
+    for indice, professor in enumerate(professores["professor"]):
+        if professor["id"] == id_professor:
+            professores["professor"].pop(indice)
+            return {"mensagem": "Professor deletado com sucesso"} # Correção: Retorno estava com ponto final extra
+    raise ProfessorNaoIdentificado()
+
+def procurar_aluno_por_id(id_aluno):
+    for aluno in dados["alunos"]:
+        if aluno["Id"] == id_aluno:
+            return aluno
+    raise AlunoNaoIdentificado()
+
+def criar_novo_aluno(novo_aluno):
+    dados["alunos"].append(novo_aluno)
+    return
+
+def listar_alunos():
+    return dados["alunos"]
+
+def deletar_aluno_por_id(id_aluno):
+    alunos = dados["alunos"]
+    for indice, aluno in enumerate(alunos):
+        if aluno["Id"] == id_aluno:
+            alunos.pop(indice)
+            return {"Mensagem": "Aluno deletado com sucesso."}
+    raise AlunoNaoIdentificado()
+
+def aluno_ja_existe(id_aluno):
+    for aluno in dados["alunos"]:
+        if aluno["Id"] == id_aluno:
+            return True
+    return False
+
+def alterar_informacoes_aluno(id_aluno, nome, idade, turma_id, data_nascimento, nota_primeiro_semestre, nota_segundo_semestre): #NÃO SEI SE VAI PRECISAR
+    try:
+        for aluno in dados["alunos"]:
+            if aluno["Id"] == id_aluno:
+                aluno["Nome"] = nome
+                aluno["Idade"] = idade
+                aluno["Turma_Id"] = turma_id
+                aluno["Data_nascimento"] = data_nascimento
+                aluno["Nota_Primeiro_Semestre"] = nota_primeiro_semestre
+                aluno["Nota_Segundo_semestre"] = nota_segundo_semestre
+                return {"Detalhes": "Aluno atualizado com sucesso!"}, 200
+        raise AlunoNaoIdentificado()
+    except Exception as e:
+        return {"Erro": "Não foi possível atualizar o aluno", "Descrição": str(e)}, 500
+                
+# Todas as rotas:
+
+@app.route("/reseta", methods=["POST","DELETE"])
+def reseta():
+    apaga_tudo()
+    return "resetado" 
+
 
 @app.route("/Turma",methods=["GET"])                              
 def listar_turma():
@@ -236,71 +351,16 @@ def AlterarInfo(id_turma):
             "Descrição": "O campo Professor Id é obrigatório se preechido"
         }), 400
     
-
-
-    # Chama a função AlterarInformacoes e retorna a resposta
     resultado, status_code = AlterarInformacoes(id_turma, dados["Descrição"], dados["Ativa"], dados["Professor Id"])
-    return jsonify(resultado), status_code      #Aqui eu quase chorei
+    return jsonify(resultado), status_code      
 
-
-
-
-
-professores = {"professor": [
-    {"id": 10, "nome": "Caio", "idade": 27, "materia": "Dev API E Micros", "obs": "Contato com aluno via Chat"},
-    {"id": 11, "nome": "Odair", "idade": 30, "materia": "DevOps", "obs": None}
-]}
-
-#devido erro persistente de 11 != 12, criou-se um def para ver se para / já a 2 dias neste erro
-def gerar_novo_id():
-    '''Criação de id, obrigatório'''
-    if not professores["professor"]: # Correção: A verificação deve ser na lista de professores
-        return 1
-    return max(professor["id"] for professor in professores["professor"]) + 1 # Correção: Iterar sobre a lista correta
-
-class ProfessorNaoIdentificado(Exception):
-    def __init__(self, msg="Not Found - Professor inexistente"):
-        self.msg = msg
-        super().__init__(self.msg)
-
-class ProfessorExistente(Exception):
-    def __init__(self, msg="Professor já existente"):
-        self.msg = msg
-        super().__init__(self.msg)
-
-class CadastroDeProfessorFalhado(Exception): # Correção: Nome da classe estava incorreto na chamada do except
-    def __init__(self, msg="ID, nome e matéria são obrigatórios"):
-        self.msg = msg
-        super().__init__(self.msg)
-
-
-def procurarProfessorPorId(id_professor):   #def é minúscula
-    for professor in professores["professor"]:
-        if professor['id'] == id_professor:
-            return professor
-    raise ProfessorNaoIdentificado()
-
-def criarNovoProfessor(nv_dict):
-    professores["professor"].append(nv_dict)
-    return
-
-def deletarProfessorPorId(id_professor):
-    for indice, professor in enumerate(professores["professor"]):
-        if professor["id"] == id_professor:
-            professores["professor"].pop(indice)
-            return {"mensagem": "Professor deletado com sucesso"} # Correção: Retorno estava com ponto final extra
-    raise ProfessorNaoIdentificado()
-
-#tratamento de execeção foi atualizado para ficar mais próximo ao feito na turma
-#lembrando que, a segui, try - funções que dão certo, except - retono de erro
-#chamar função correspondente dentro da rota
 
 @app.route('/professores', methods=['GET'])
 def listar_professores():
     try:
-        return jsonify({"mensagem": "Ok", "professores": professores["professor"]}) #correção do retorno
+        return jsonify({"mensagem": "Ok", "professores": professores["professor"]}) 
     except Exception as e:
-        return jsonify({"error": f"Internal Server Error: {str(e)}"}), 500 # Correção: Mensagem de erro mais clara
+        return jsonify({"error": f"Internal Server Error: {str(e)}"}), 500 
 
 @app.route("/professores/<int:id>", methods=["GET"])
 def pesquisa_professor(id):
@@ -349,79 +409,6 @@ def delete_professor(id_professor):
         return jsonify(resultado), 200
     except ProfessorNaoIdentificado as e:
         return jsonify({"erro": str(e)}), 404
-
-if __name__ == '__main__':
-    app.run(debug=True)
-
-
-###ALUNO###
-
-# Criando classes para minhas exceções:
-class AlunoNaoIdentificado(Exception):
-    def _init_(self, msg="Erro, Aluno não identificado ou inexistente!"):
-        self.msg = msg
-        super()._init_(self.msg)
-
-class AlunoExistente(Exception):
-    def _init_(self, msg="Erro, Aluno já existente!"):
-        self.msg = msg
-        super()._init_(self.msg)
-
-class CadastroDeAlunoFalhado(Exception):
-    def _init_(self, msg="Erro, Id do aluno ou Turma_Id incorretos!"):
-        self.msg = msg
-        super()._init_(self.msg)
-
-class AtualizacaoAlunoFalhou(Exception):
-    def _init_(self, msg="Erro, Não foi possível atualizar os dados do aluno! Reveja os campos e preencha corretamente"):
-        self.msg = msg
-        super()._init_(self.msg)
-
-
-
-
-# Aqui estão as funções auxiliares para as rotas:
-def procurar_aluno_por_id(id_aluno):
-    for aluno in dados["alunos"]:
-        if aluno["Id"] == id_aluno:
-            return aluno
-    raise AlunoNaoIdentificado()
-
-def criar_novo_aluno(novo_aluno):
-    dados["alunos"].append(novo_aluno)
-    return
-
-def listar_alunos():
-    return dados["alunos"]
-
-def deletar_aluno_por_id(id_aluno):
-    alunos = dados["alunos"]
-    for indice, aluno in enumerate(alunos):
-        if aluno["Id"] == id_aluno:
-            alunos.pop(indice)
-            return {"Mensagem": "Aluno deletado com sucesso."}
-    raise AlunoNaoIdentificado()
-
-def aluno_ja_existe(id_aluno):
-    for aluno in dados["alunos"]:
-        if aluno["Id"] == id_aluno:
-            return True
-    return False
-
-def alterar_informacoes_aluno(id_aluno, nome, idade, turma_id, data_nascimento, nota_primeiro_semestre, nota_segundo_semestre): #NÃO SEI SE VAI PRECISAR
-    try:
-        for aluno in dados["alunos"]:
-            if aluno["Id"] == id_aluno:
-                aluno["Nome"] = nome
-                aluno["Idade"] = idade
-                aluno["Turma_Id"] = turma_id
-                aluno["Data_nascimento"] = data_nascimento
-                aluno["Nota_Primeiro_Semestre"] = nota_primeiro_semestre
-                aluno["Nota_Segundo_semestre"] = nota_segundo_semestre
-                return {"Detalhes": "Aluno atualizado com sucesso!"}, 200
-        raise AlunoNaoIdentificado()
-    except Exception as e:
-        return {"Erro": "Não foi possível atualizar o aluno", "Descrição": str(e)}, 500
 
 
 # Aqui estão todas as rotas:
@@ -490,45 +477,4 @@ def alterar_aluno_route(id_aluno):
 
 
 if __name__ == '__main__':
-    app.run(host = 'localhost', port = 5002, debug=True)
-
-
-
-
-
-     
-
-
-
-# app = Flask(__name__)
-# @app.route('/professores', methods=['GET'])
-# def listar_professores():
-#     return jsonify(professores), 200
-
-# @app.route('/professores', methods=['POST'])
-# def cadastrar_professores():
-#     dados = request.json
-#     novo_professor = {
-#         'id': len(professores) +1,
-#         'nome':dados.get('nome')
-
-        
-#     }
-#     professores.append(novo_professor)
-#     return jsonify(novo_professor), 201
-    
-#     @app.route('/profesores/<int:id>', methods=['PUT'])
-#     def atualizar_professor(id):
-#         dados = request.json
-#         for professor in professores:
-#         f professor['id'] == id:
-#             professor['nome'] = dados.get('nome', professor['nome'])
-#             professor['disciplina'] = dados.get('disciplina', professor['disciplina'])
-#             return jsonify(professor), 200
-#     return jsonify({'erro': 'Professor não encontrado'}), 404
-
-if __name__ == '__main__':
     app.run(debug=True)
-
-
-
