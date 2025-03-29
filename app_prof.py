e87b5a389a419fb5d328df738aa019e294f0ef6a
Alessandra F Rigonatti <alessandra.rigonatti@aluno.faculdadeimpacta.com.br>
2025-03-28 22:02:18 -0300

    app_prof| model_professor

--- app_prof.py
+++ app_prof.py
@@ -9,7 +9,7 @@ def reseta():
     apaga_tudo()
     return "resetado" 
 
-# apenas professores - juntar- se correto à app.py
+# apenas professores - juntar, se correto, à app.py
 
 @app.route('/professores', methods=['GET'])
 def listar_professores():
@@ -25,7 +25,7 @@ def listar_professores():
 @app.route("/professores/<int:id>", methods=["GET"])
 def pesquisa_professor(id):
     
-    '''Devolve dict com base no id (chave/pk)'''
+    '''Devolve dict professore/professor com base no id (chave/pk)'''
     
     try:
         professor = procurarProfessorPorId(id)
@@ -33,7 +33,8 @@ def pesquisa_professor(id):
     except ProfessorNaoIdentificado as e:
         return jsonify({"erro": str(e)}), 404
     
-###########################################
+# ------------------- # --------------------------------------#
+
 @app.route('/professores', methods=['POST'])
 def cadastrar_professores():
     
@@ -54,10 +55,13 @@ def cadastrar_professores():
         return jsonify({"erro": str(e)}), 400
     #pode ser erro 409 - conflict - conflito entre dados ou dados iguais?
     
-###############################################################################################################
+# ------------------- # --------------------------------------#
 
 @app.route('/professores/<int:id>', methods=['PUT'])
 def atualizar_professor(id):
+    
+    ''' Atualização dos dados de professor já existentes'''
+    
     atualizado = request.json
     try:
         professor = procurarProfessorPorId(id) # Correção: Usar a função de procurarProfessorPorId correta
@@ -75,88 +79,28 @@ def atualizar_professor(id):
     except Exception as e:
         return jsonify({"erro": f"Internal Server Error: {str(e)}"}) # Correção: Mensagem de erro mais clara
 
+# ------------------- # --------------------------------------#
+
 @app.route("/professores/deletar/<int:id_professor>", methods=["DELETE"])
 def delete_professor(id_professor):
+    
+    '''Apagar dados de professor já existente'''
     try:
         resultado = deletarProfessorPorId(id_professor)
         return jsonify(resultado), 200
     except ProfessorNaoIdentificado as e:
         return jsonify({"erro": str(e)}), 404
 
+# ------------------- # --------------------------------------#
+
 @app.route('/professores/resetar', methods=['DELETE'])
 def resetar_professor():
+    
+    '''Restaurar do zero a lista professores'''
+    
     resetar_professores()  # Função que reseta o dicionário de professores
     return jsonify({"mensagem": "Resetado"}), 200
 
-
-@app.route("/alunos", methods=["GET"])
-def listar_alunos_route():
-    alunos = listar_alunos()
-    return jsonify(alunos)
-
-@app.route("/alunos/<int:id_aluno>", methods=["GET"])
-def procurar_aluno_route(id_aluno):
-    try:
-        aluno = procurar_aluno_por_id(id_aluno)
-        return jsonify(aluno)
-    except AlunoNaoIdentificado as e:
-        return jsonify({"Erro": str(e)}), 404
-
-@app.route("/alunos", methods=["POST"])
-def adicionar_aluno():
-    novo_aluno = request.json
-    novo_aluno["Id"] = int(novo_aluno["Id"])
-    novo_aluno["Turma_Id"] = int(novo_aluno["Turma_Id"])
-
-    try:
-        if aluno_ja_existe(novo_aluno["Id"]):
-            raise AlunoExistente()
-        criar_novo_aluno(novo_aluno)
-        return jsonify({"mensagem": "Aluno criado com sucesso!", "aluno": novo_aluno}), 201
-    except AlunoExistente as es:
-        return jsonify({"Erro": str(es)}), 400
-
-
-@app.route("/alunos/deletar/<int:id_aluno>", methods=["DELETE"])
-def deletar_aluno_route(id_aluno):
-    try:
-        resultado = deletar_aluno_por_id(id_aluno)
-        return jsonify(resultado), 200
-    except AlunoNaoIdentificado as e:
-        return jsonify({"Erro": str(e)}), 404
-    
-@app.route('/alunos/resetar', methods=['DELETE'])
-def resetar_alunoId():
-    deletar_alunos()
-    return jsonify({"mensagem": "Resetado"}), 200
-
-
-@app.route("/alunos/<int:id_aluno>", methods=["PUT"])
-def alterar_aluno_route(id_aluno):
-    dados_aluno = request.json
-
-    if not dados_aluno:
-        return jsonify({
-            "Erro": "Requisição inválida",
-            "Descrição": "O corpo da requisição está vazio, preencha todos os campos"
-        }), 400
-
-    try:
-        resultado, status_code = alterar_informacoes_aluno(
-            id_aluno,
-            dados_aluno.get("Nome"),
-            dados_aluno.get("Idade"),
-            dados_aluno.get("Turma_Id"),
-            dados_aluno.get("Data_nascimento"),
-            dados_aluno.get("Nota_Primeiro_Semestre"),
-            dados_aluno.get("Nota_Segundo_semestre"),
-            dados_aluno.get("Media_final")
-        )
-        return jsonify(resultado), status_code
-    except AlunoNaoIdentificado as e:
-        return jsonify({"Erro": str(e)}), 404
-    except Exception as e:
-        return jsonify({"Erro": "Falha ao atualizar aluno", "Detalhes": str(e)}), 500
 
 
 if __name__ == '__main__':
--- model_professor.py
+++ model_professor.py
@@ -49,26 +49,6 @@ professores = {"professor": [
 #Criando todas as classes de exceções:
 
 
-class AlunoNaoIdentificado(Exception):
-    def _init_(self, msg="Erro, Aluno não identificado ou inexistente!"):
-        self.msg = msg
-        super()._init_(self.msg)
-
-class AlunoExistente(Exception):
-    def _init_(self, msg="Erro, Aluno já existente!"):
-        self.msg = msg
-        super()._init_(self.msg)
-
-class CadastroDeAlunoFalhado(Exception):
-    def _init_(self, msg="Erro, Id do aluno ou Turma_Id incorretos!"):
-        self.msg = msg
-        super()._init_(self.msg)
-
-class AtualizacaoAlunoFalhou(Exception):
-    def _init_(self, msg="Erro, Não foi possível atualizar os dados do aluno! Reveja os campos e preencha corretamente"):
-        self.msg = msg
-        super()._init_(self.msg)
-
 class ProfessorNaoIdentificado(Exception):
     def __init__(self, msg="Not Found - Professor inexistente"):
         self.msg = msg
@@ -110,58 +90,12 @@ def deletarProfessorPorId(id_professor):
             professores["professor"].pop(indice)
             return {"mensagem": "Professor deletado com sucesso"} # Correção: Retorno estava com ponto final extra
     raise ProfessorNaoIdentificado()
-
-def procurar_aluno_por_id(id_aluno):
-    for aluno in dados["alunos"]:
-        if aluno["Id"] == id_aluno:
-            return aluno
-    raise AlunoNaoIdentificado()
-
-def criar_novo_aluno(novo_aluno):
-    dados["alunos"].append(novo_aluno)
-    return {"Resposta":"Aluno criando com sucesso"}
-
-def listar_alunos():
-    return dados["alunos"]
-
-def deletar_aluno_por_id(id_aluno):
-    alunos = dados["alunos"]
-    for indice, aluno in enumerate(alunos):
-        if aluno["Id"] == id_aluno:
-            alunos.pop(indice)
-            return {"Mensagem": "Aluno deletado com sucesso."}
-    raise AlunoNaoIdentificado()
-
-def aluno_ja_existe(id_aluno):
-    for aluno in dados["alunos"]:
-        if aluno["Id"] == id_aluno:
-            return True
-    return False
-
-def alterar_informacoes_aluno(id_aluno, nome, idade, turma_id, data_nascimento, nota_primeiro_semestre, nota_segundo_semestre, media_final): #NÃO SEI SE VAI PRECISAR
-    try:
-        for aluno in dados["alunos"]:
-            if aluno["Id"] == id_aluno:
-                aluno["Nome"] = nome
-                aluno["Idade"] = idade
-                aluno["Turma_Id"] = turma_id
-                aluno["Data_nascimento"] = data_nascimento
-                aluno["Nota_Primeiro_Semestre"] = nota_primeiro_semestre
-                aluno["Nota_Segundo_semestre"] = nota_segundo_semestre
-                aluno["Media_final"] = media_final
-                return {"Detalhes": "Aluno atualizado com sucesso!"}, 200
-        raise AlunoNaoIdentificado()
-    except Exception as e:
-        return {"Erro": "Não foi possível atualizar o aluno", "Descrição": str(e)}, 500
     
 def resetar_professores():
     professores["professor"] = []
     return
 
-def deletar_alunos():
-    dados["alunos"] = []
-    return
-                
+
 # Todas as rotas: São Controller 
 
 if __name__ == '__main__':
