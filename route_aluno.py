1e645c0d234573f57b844036e59644ceb599dacb
Thainá Foltran <thainafoltran@hotmail.com>
2025-04-07 20:08:10 -0300

    add blueprint rota aluno get por id

--- route_aluno.py
+++ route_aluno.py
@@ -7,3 +7,11 @@ bp_aluno = Blueprint("alunos", __name__)
 def listar_alunos_route():
     alunos = modal.listar_alunos()
     return jsonify(alunos)
+
+@bp_aluno.route("/alunos/<int:id_aluno>", methods=["GET"])
+def procurar_aluno_route(id_aluno):
+    try:
+        aluno = modal.modprocurar_aluno_por_id(id_aluno)
+        return jsonify(aluno)
+    except modal.AlunoNaoIdentificado as e:
+        return jsonify({"Erro": str(e)}), 404
