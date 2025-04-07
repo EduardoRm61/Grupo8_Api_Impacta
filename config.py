2a5a6a458c1909fc072e8db3ca700d06ae1f720b
Rafaelawr <rafaelawr@hotmail.com>
2025-04-07 08:18:55 -0300

    config criado

---
+++ config.py
@@ -0,0 +1,12 @@
+from flask import Flask, jsonify, request
+from config_prof import app
+from route_prof import bp_professor
+from rout_alu import blueprint_aluno
+from rout_turma import blueprint_turma
+
+app.register_blueprint(bp_professor)
+app.register_blueprint(blueprint_aluno)
+app.register_blueprint(blueprint_turma)
+
+if _name_ == "_main_":
+    app.run(host = app.config["HOST"], port = app.config["PORT"], debug = app.config["DEBUG"])
