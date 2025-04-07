6b9683191f6635bc9b2f1e2fdf273ef96515c92a
Rafaelawr <rafaelawr@hotmail.com>
2025-04-07 17:29:24 -0300

    retirando

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ from config import app
 from rout_prof import bp_professor
 
 app.register_blueprint(bp_professor)
-app.register_blueprint(professor)
 
 if __name__ == '__main__':
   app.run(host=app.config["HOST"], port = app.config['PORT'],debug=app.config['DEBUG'] )
