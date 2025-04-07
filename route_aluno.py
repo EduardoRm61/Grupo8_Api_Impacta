0e2094af38b4be1ef3785e2ebfc0c2dd5ef36de8
Thainá Foltran <thainafoltran@hotmail.com>
2025-04-07 20:15:42 -0300

    feat adicionei a rota post de alunos

--- route_aluno.py
+++ route_aluno.py
@@ -1,17 +1,32 @@
 from flask import Blueprint, request, jsonify
-import model_aluno as modal
+import model_aluno as modAl
 
 bp_aluno = Blueprint("alunos", __name__)
 
 @bp_aluno.route("/alunos", methods=["GET"])
 def listar_alunos_route():
-    alunos = modal.listar_alunos()
+    alunos = modAl.listar_alunos()
     return jsonify(alunos)
 
 @bp_aluno.route("/alunos/<int:id_aluno>", methods=["GET"])
 def procurar_aluno_route(id_aluno):
     try:
-        aluno = modal.modprocurar_aluno_por_id(id_aluno)
+        aluno = modAl.modprocurar_aluno_por_id(id_aluno)
         return jsonify(aluno)
-    except modal.AlunoNaoIdentificado as e:
+    except modAl.AlunoNaoIdentificado as e:
         return jsonify({"Erro": str(e)}), 404
+    
+
+@bp_aluno.route("/alunos", methods=["POST"])
+def adicionar_aluno():
+    novo_aluno = request.json
+    novo_aluno["Id"] = int(novo_aluno["Id"])
+    novo_aluno["Turma_Id"] = int(novo_aluno["Turma_Id"])
+    try:
+        if modAl.aluno_ja_existe(novo_aluno["Id"]):
+            raise modAl.AlunoExistente()
+        modAl.criar_novo_aluno(novo_aluno)
+        return jsonify({"mensagem": "Aluno criado com sucesso!", "aluno": novo_aluno}), 201
+    except modAl.AlunoExistente as es:
+        return jsonify({"Erro": str(es)}), 400
+
