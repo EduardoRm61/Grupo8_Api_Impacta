33ae05fb50b9ceffbdc72bd0af5f561c335febe8
Eduardo N. <eduardonunesdasilva23@gmail.com>
2025-04-07 22:27:13 -0300

    move movendo rota Turma POST para routes

--- routes_turma.py
+++ routes_turma.py
@@ -21,4 +21,31 @@ def procurarTurma(id_turma):
           return jsonify({"Erro:": str(trm)}), 402  #Isso precisa ser alterado
 
 
+@Blueprint.route("/Turma", methods=["POST"])
+def AddTurma():
+    nv_dict = request.json
+    nv_dict['Id'] = int(nv_dict['Id'])
+    nv_dict['Professor Id'] = int(nv_dict['Professor Id'])
+
+    try:
+         
+        if not modTur.ProfessorExistente(nv_dict["Professor Id"]):
+            return jsonify({
+                "Erro": "Requisição inválida",
+                "Detalhes": "Id do Professor inexistente"
+            }), 404   #inexistente | estava bad request
+
+        if modTur.TurmaJaExiste(nv_dict["Id"]):
+            return jsonify({
+                "Erro": "Requisição inválida",
+                "Detalhes": "Id da Turma já existente"
+            }), 409  #conflict - duplicado ou duplo - estava 400 bad request
+        modTur.CriarNovaTurma(nv_dict)
+        return jsonify({"mensagem": "Turma criada com sucesso!", "turma": nv_dict}), 201
+    
+    except modTur.CadastroDeTurmaFalhado as cdtf:
+         return jsonify({
+            "Erro": "Falha ao cadastrar turma",
+            "detalhes": str(cdtf)
+         }), 400    
 
