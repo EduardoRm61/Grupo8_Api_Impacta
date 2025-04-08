558b93c9decefde252a7229d879b23a777506b7e
Eduardo N. <eduardonunesdasilva23@gmail.com>
2025-04-07 22:19:07 -0300

    move update Turma por Id, alterando e movendo

--- routes_turma.py
+++ routes_turma.py
@@ -11,5 +11,14 @@ def listar_turma():
         return jsonify(Turmas)
     except modTur.TurmaNaoIdentificada as Tr:
         return {"Requisição inválida":str(Tr)}, 400
+    
+@Blueprint.route("/Turma/<int:id_turma>", methods=["GET"])            
+def procurarTurma(id_turma):
+     try:
+        dados = modTur.ProcurarTurmaPorId(id_turma)
+        return jsonify(dados)
+     except modTur.TurmaNaoIdentificada as trm:
+          return jsonify({"Erro:": str(trm)}), 402  #Isso precisa ser alterado
+
 
 
