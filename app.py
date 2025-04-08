03dea61735f6aa974cf28bbaa92d5ea3ae794f2e
Alessandra F Rigonatti <alessandra.rigonatti@aluno.faculdadeimpacta.com.br>
2025-04-08 17:30:04 -0300

    add import b lueprint app.py turma aluno

--- app.py
+++ app.py
@@ -2,8 +2,14 @@ import os
 from flask import Flask, jsonify, request
 from config import app
 from route_prof import bp_professor
+from routes_turma import Bd_Turma
+from route_aluno import bp_aluno
+
 
 app.register_blueprint(bp_professor)
+app.register_blueprint(Bd_Turma)
+app.register_blueprint(bp_aluno)
+
 
 if __name__ == '__main__':
   app.run(host=app.config["HOST"], port = app.config['PORT'],debug=app.config['DEBUG'] )
---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+app.config['HOST'] = 'localhost'
+app.config['PORT']= 5002
+app.config['DEBUG'] = True
