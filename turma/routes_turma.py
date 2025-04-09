ad99c2f1d28253a685563c36395022c47772db85
Eduardo N. <eduardonunesdasilva23@gmail.com>
2025-04-08 23:12:42 -0300

    update de todas as chamadas de funções nas routes_turma.py

--- turma/routes_turma.py
+++ turma/routes_turma.py
@@ -7,7 +7,7 @@ Bd_Turma = Blueprint('Turma', __name__ )
 @Bd_Turma.route("/Turma",methods=["GET"])                              
 def listar_turma():
     try:
-        Turmas = modTur.ListarTurma()
+        Turmas = modTur.listarTurma()
         return jsonify(Turmas)
     except modTur.TurmaNaoIdentificada as Tr:
         return jsonify ({"Requisição inválida":str(Tr)}), 400
@@ -15,7 +15,7 @@ def listar_turma():
 @Bd_Turma.route("/Turma/<int:id_turma>", methods=["GET"])            
 def procurarTurma(id_turma):
      try:
-        dados = modTur.ProcurarTurmaPorId(id_turma)
+        dados = modTur.procurarTurmaPorId(id_turma)
         return jsonify(dados)
      except modTur.TurmaNaoIdentificada as trm:
           return jsonify({"Erro:": str(trm)}), 402  #Isso precisa ser alterado
@@ -29,18 +29,18 @@ def AddTurma():
 
     try:
          
-        if not modTur.ProfessorExistente(nv_dict["Professor Id"]):
+        if not modTur.professorExistente(nv_dict["Professor Id"]):
             return jsonify({
                 "Erro": "Requisição inválida",
                 "Detalhes": "Id do Professor inexistente"
             }), 404   #inexistente | estava bad request
 
-        if modTur.TurmaJaExiste(nv_dict["Id"]):
+        if modTur.turmaJaExiste(nv_dict["Id"]):
             return jsonify({
                 "Erro": "Requisição inválida",
                 "Detalhes": "Id da Turma já existente"
             }), 409  #conflict - duplicado ou duplo - estava 400 bad request
-        modTur.CriarNovaTurma(nv_dict)
+        modTur.criarNovaTurma(nv_dict)
         return jsonify({"mensagem": "Turma criada com sucesso!", "turma": nv_dict}), 201
     
     except modTur.CadastroDeTurmaFalhado as cdtf:
@@ -53,7 +53,7 @@ def AddTurma():
 @Bd_Turma.route("/Turma/Resetar", methods=["DELETE"])
 def ResetarTodaTurma():
     try:
-        modTur.DeletarTurma()
+        modTur.deletarTurma()
         return jsonify({"mensagem": "Resetado"}), 200
     except modTur.TurmaJaDeletada as Trm:
         return jsonify ({"Requisção Inválida": str(Trm)}), 400 
@@ -61,7 +61,7 @@ def ResetarTodaTurma():
 @Bd_Turma.route("/Turma/Resetar/<int:id_turma>", methods=["DELETE"])
 def ResetarTurmaId(id_turma):
      try:
-          modTur.DeletarTurmaPorId(id_turma)
+          modTur.deletarTurmaPorId(id_turma)
           return jsonify(modTur.dadosTurma["Turma"]), 200
      except modTur.TurmaNaoIdentificada as trm:
           return jsonify({"Erro:": str(trm)}), 404
@@ -96,5 +96,5 @@ def AlterarInfo(id_turma):
             "Descrição": "O campo Professor Id é obrigatório se preechido"
         }), 400
     
-    resultado, status_code = modTur.AlterarInformacoes(id_turma, dados["Descrição"], dados["Ativa"], dados["Professor Id"])
+    resultado, status_code = modTur.alterarInformacoes(id_turma, dados["Descrição"], dados["Ativa"], dados["Professor Id"])
     return jsonify(resultado), status_code  
