7764652d3ff1a6d887e2d066e59a810571a8a99b
Alessandra F Rigonatti <alessandra.rigonatti@aluno.faculdadeimpacta.com.br>
2025-04-28 01:01:12 -0300

    add model.py mative class def professorExistente def procurarProfessorId def criarNovoProfessor def atualizarProfessor

--- apps/professores/model_prof.py
+++ apps/professores/model_prof.py
@@ -2,7 +2,7 @@
 
 from config import db_serv
 
-
+# __________________INSTÂNCIANDO________________________________________
    
 class Professor(db_serv.Model):
     
@@ -25,7 +25,7 @@ class Professor(db_serv.Model):
         self.materia = materia
         self.obs = obs
     
-    def direcionar (self):
+    def direcionar(self):
         
         '''função que relaciona a chave com dados, formando retorno de um dicinário'''
         
@@ -37,6 +37,7 @@ class Professor(db_serv.Model):
             "obs" : self.obs
         }
 
+# ____________ CLASSE DE EXCEÇÃO NÃO É DADOS- DEIXAR________________________
 
 class ProfessorNaoIdentificado(Exception):
     def __init__(self,msg="Erro, Professor não indentificado ou existente!"):
@@ -53,32 +54,50 @@ class CadastroDeProfessorFalhado(Exception): # Correção: Nome da classe estava
         self.msg = msg
         super().__init__(self.msg)
 
-def apaga_tudo():
-     professores["Professor"] = []
+# _________________________ FUNÇÕES________________________
+
+# def apaga_tudo():
+#      professores["Professor"] = []
 
 def ProfessorExistente(Id_professor):
-    for professor in professores["professor"]:
-        if professor["id"] == Id_professor:
-            return True  
-    return False
+    return Professor.query.get(Id_professor) is not None
 
 
 def procurarProfessorPorId(id_professor):   #def é minúscula
-    for professor in professores["professor"]:
-        if professor['id'] == id_professor:
-            return professor
-    raise ProfessorNaoIdentificado()
+    professor = Professor.query.get(id_professor)
+    if not professor:
+        raise ProfessorNaoIdentificado()
+    return professor.direcionar()
 
-def criarNovoProfessor(nv_dict):
-    professores["professor"].append(nv_dict)
-    return
+def criarNovoProfessor(new_direcionar):
+    if not new_direcionar.get("id") or not new_direcionar.get("nome") or not new_direcionar.get("materia"):
+    # se não tiver 1 ou 2 ou todos, falha
+        raise CadastroDeProfessorFalhado()
+    
+    if ProfessorExistente(new_direcionar["id"]):
+        raise ProfessorExiste()
+    
+    novo_professor = Professor (
+        id=new_direcionar["id"],
+        nome=new_direcionar["nome"],
+        materia=new_direcionar["materia"],
+        idade=new_direcionar.get("idade"),
+        obs=new_direcionar.get("obs")
+    )
+        
+    db_serv.session.add(novo_professor)  # add o objeto à sessão do banco de dados
+    db_serv.session.commit()             # confirma a transação, grava o objeto no banco de dados mysql
+    return novo_professor.to_dict()      # converte em diconário
+    
+    #cuidado com identação e abrir e fechar ( )
 
 def deletarProfessorPorId(id_professor):
-    for indice, professor in enumerate(professores["professor"]):
-        if professor["id"] == id_professor:
-            professores["professor"].pop(indice)
-            return {"mensagem": "Professor deletado com sucesso"} # Correção: Retorno estava com ponto final extra
-    raise ProfessorNaoIdentificado()
+    professor = Professor.query.get(id_professor)
+    if not professor:
+        raise ProfessorNaoIdentificado
+    
+    db_serv.session.delete(professor)
+    db_serv.session.commit()
 
 def resetar_professores():
     professores["professor"] = []
