57262e535548f50078e9d0b8ed00e85acff5219b
Eduardo N. <eduardonunesdasilva23@gmail.com>
2025-04-28 22:13:34 -0300

    teste

--- apps/app.py
+++ apps/app.py
@@ -4,21 +4,20 @@ from config import app
 from professores.route_prof import bp_professor
 from turma.routes_turma import Bd_Turma
 from alunos.route_aluno import bp_aluno
-from flask_sqlalchemy import SQLAlchemy
+
 
 app.register_blueprint(bp_professor)
 app.register_blueprint(Bd_Turma)
 app.register_blueprint(bp_aluno)
 
-#Aqui eu estou criando o Objeto db
-db = SQLAlchemy
+
 
 # Criando o app
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://adm:12345@db:3306/SistemaEscolar'
 
 # Inicializando o Banco com o app
-db.init_app(app)
+#db.init_app(app)
 
 
 if __name__ == '__main__':
--- apps/config.py
+++ apps/config.py
@@ -1,5 +1,6 @@
 import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
@@ -10,3 +11,10 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Desativando o rastreamen
 app.config['HOST'] = '0.0.0.0'
 app.config['PORT']= 5002
 app.config['DEBUG'] = True
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://adm:12345@db:3306/SistemaEscolar'
+#app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://${adm}:${12345}@${db}/${dados_mysql}"
+
+#Aqui eu estou criando o Objeto db
+db = SQLAlchemy(app)
