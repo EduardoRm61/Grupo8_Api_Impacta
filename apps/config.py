1b65dc4d8f8df9ee02b902064c25aa662c76193c
Alessandra F Rigonatti <alessandra.rigonatti@aluno.faculdadeimpacta.com.br>
2025-05-02 16:28:02 -0300

    add config.py

--- apps/config.py
+++ apps/config.py
@@ -1,4 +1,4 @@
-import os   #porque impostar os se não estou usando?
+import os   
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
@@ -8,19 +8,7 @@ app.config["HOST"] = "0.0.0.0"  # 127.0.0.1 - apenas host local, mudei para 0.0.
 app.config["PORT"]= 5002
 app.config["DEBUG"] = True
 
-#-----------------------------------------------------------------------------------------------------------#
-#                                   Conexão com .env - usar mysql                                           #
-#--------------------------------------------------------------------------------------------------------#
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+app.config["SQLACHEMY_TRACK_MODIFICATIONS"] = False
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://${adm}:${12345}@${db}/${dados_mysql}"  # como será a conexão do flask ao mysql
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-
-
-# DB_HOST: db  # Nome do serviço MySQL no compose
-# DB_NAME: dados_mysql
-# DB_USER: adm
-# DB_PASSWORD: 12345
-# "mysql+pymysql://DB_USER:DB_PASSWORD@DB_HOST/DB_NAME"
-# ESTÁ EM APP: ENVIRONMENT
-
-db_serv = SQLAlchemy(app)   #instanciando/criando objeto do SQLAlchemy vinculado ao app/flask
+db_serv = SQLAlchemy(app)
