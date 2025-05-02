62f1b4921e288198664b374ecada5142243a4fac
Alessandra F Rigonatti <alessandra.rigonatti@aluno.faculdadeimpacta.com.br>
2025-05-02 16:24:45 -0300

    add app.py cod

--- app.py
+++ app.py
@@ -1,48 +1,37 @@
 
 import sys
-
 import os
-from flask import Flask, jsonify, request
-from config import app                                      # estava apps.config import ...
-from swagger.swagger_config import configure_swagger
+from flask import Flask, jsonify, request, redirect, url_for
+from apps.config import app,db_serv
+from apps.swagger.swagger_config import configure_swagger
+from apps.professores.route_prof import bp_professor
+from apps.turma.routes_turma import Bd_Turma
+from apps.alunos.route_aluno import bp_aluno
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
-from swagger.swagger_config import configure_swagger
-from flask import Flask, jsonify, request
-from config import app
-from professores.route_prof import bp_professor
-from turma.routes_turma import Bd_Turma
-from alunos.route_aluno import bp_aluno
 from flask_sqlalchemy import SQLAlchemy
-from flask import redirect, url_for
 
 app.register_blueprint(bp_professor)
 app.register_blueprint(Bd_Turma)
 app.register_blueprint(bp_aluno)
-app.register_blueprint(bp_professor, url_prefix='/api')
-app.register_blueprint(Bd_Turma, url_prefix='/api')
-app.register_blueprint(bp_aluno, url_prefix='/api')
+# app.register_blueprint(bp_professor, url_prefix='/api')
+# app.register_blueprint(Bd_Turma, url_prefix='/api')
+# app.register_blueprint(bp_aluno, url_prefix='/api')
 
+api = Api(
+  app, 
+  version= "1.0", 
+  title= "Grupo8_Api_Impacta",  
+  description= "Aplicativo de gerenciamênto de dados de turmas, professor e alunos da faculdade Impacta", 
+  doc = "/docs"  
+)
+with app.app_context(): 
+  db_serv.create_all() 
 
-# app.register_blueprint(bp_professor, url_prefix="/professores" )
-# app.register_blueprint(Bd_Turma, url_prefix="/Turma")
-# app.register_blueprint(bp_aluno, url_for="/alunos")
-# para url vir com a rota professor, sem precisar escrever,<status falhou>
-
-# _________________________________________REDIRECIONAMENTO__________________________________________________
-
-# @app.route("/")
-# def redirecionamento():
-#     return redirect(url_for("professores.listar_professores"))
-
-#rafa, vc tirou este ou vc não o tinha?
-
-
-swagger_url ='/docs' #url aonde o swagger estará disponivel
-API_URL = '/static/swagger.json' #ccaminho para o arquvio json
+swagger_url ='/docs' 
+API_URL = '/static/swagger.json' 
 configure_swagger(app)
-
 
 if __name__ == '__main__':
   app.run(host=app.config["HOST"], port = app.config['PORT'],debug=app.config['DEBUG'] )
