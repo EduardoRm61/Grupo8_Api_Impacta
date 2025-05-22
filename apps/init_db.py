fa0baea5d49e423b435ea75e0288ee6f9021e08c
Eduardo N. <eduardonunesdasilva23@gmail.com>
2025-05-22 09:13:36 -0300

    corrigindo...

--- apps/init_db.py
+++ apps/init_db.py
@@ -1,6 +1,7 @@
 from config import app, db_serv
 from turma.model_turma import Turma  # caminho relativo ao init_db.py
 from professores.model_prof import Professor
+from alunos.model_aluno import Aluno
 
 with app.app_context():
     db_serv.create_all()
--- apps/swagger/swagger_config.py
+++ apps/swagger/swagger_config.py
@@ -1,6 +1,9 @@
 from . import api
 from swagger.namespace.turma_namespace import turma_ns
-
+from swagger.namespace.aluno_namespace import alunos_ns
+from swagger.namespace.professor_namespace import professor_ns
 def configure_swagger(app):
     api.add_namespace(turma_ns, path="/Turma")
+    api.add_namespace(alunos_ns, path="/alunos")
+    api.add_namespace(professor_ns, path="/professores")
     api.mask_swagger = False
